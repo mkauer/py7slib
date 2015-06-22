3998abbebd6d96e0caf6f18fb13a728fd8666df4
Huse Gomiz <gomiz03@gmail.com>
2015-06-22 17:49:29 +0200

    spiflash_update: programming has been verified
      * CRC checking
      * Add Timeout to send packets
      * Add exception handing

--- bridges/ethbone.py
+++ bridges/ethbone.py
@@ -3,6 +3,44 @@
 '''
 This file contains the etherbone class which is a child of the abstract class GenDrv (gendrvr.py)
 
+
+Parse etherbone packet with wireshark
+=====================================
+
+Copy s4n.lua protocol.
+-----------------------
+
+Then if you want to read the lua protocol you need to copy the file `spec/etherbone.lua` from etherbone repo to the wireshark directory
+
+    * Under windows: `C:\\Program Files\\Wireshark`
+    * Under ubuntu: `/usr/share/wireshark/`
+
+
+Activate LUA script.
+----------------------
+
+Go to the wireshark directory and edit the file: `init.lua`
+You need to replace the begin of the file with:
+
+run_user_scripts_when_superuser = true
+
+-- disable potentialy harmful lua functions when running superuser
+if running_superuser_back then
+
+
+And add to the end of the file the line
+
+    dofile(DATA_DIR.."etherbone.lua")
+
+Install lua libraries
+----------------------
+
+    sudo apt-get install lua5.1 lua-bitop
+
+Call back:
+http://stackoverflow.com/questions/7259794/how-can-i-get-methods-to-work-as-callbacks-with-python-ctypes
+
+
 @file
 @date Created on Jun 16, 2015
 @author Benoit Rat (benoit<AT>sevensols.com)
@@ -32,16 +70,28 @@ This file contains the etherbone class which is a child of the abstract class Ge
 # Import system modules
 import subprocess
 import os
+import sys
 from ctypes import *
+import time
+import struct
+
+
+
+
 
 # Import common modules
 from gendrvr import *
+import binascii
 
 EB_PROTOCOL_VERSION = 1
 EB_ABI_VERSION      = 0x04
-EB_BUS_MODEL        = 0x44
+EB_BUS_MODEL        = [0x44,0x88][sys.maxsize > 2**32]
 EB_MEMORY_MODEL     = 0x0000
 EB_ABI_CODE         = ((EB_ABI_VERSION << 8) + EB_BUS_MODEL + EB_MEMORY_MODEL)
+
+def py_cb_func(user, dev, op, status):
+     if status: raise NameError('Callback Error: %s' % (status))
+     print "%x" %(status)
 
 # Import Etherbone structures
 class eb_handler(Structure):
@@ -81,6 +131,7 @@ class EthBone(GenDrvr):
             LUN : the logical unit, in etherbone we use a netaddress format given by:
             show_dbg : enables debug info
         '''
+
         print os.getenv('LD_LIBRARY_PATH')
         self.load_lib("libetherbone.so")
 
@@ -88,6 +139,7 @@ class EthBone(GenDrvr):
         self.socket    = c_uint(0)
         self.device    = c_uint(0)
         self.operation = c_uint(0)
+        self.wcrc      = 0
 
         ##Setup arguments
         self.LUN=LUN
@@ -98,11 +150,15 @@ class EthBone(GenDrvr):
         self.addr_width=self.EB_ADDRX
         self.data_width=self.EB_DATAX
         self.attempts=3
+        self.silent=True
         self.format=c_uint8(self.EB_BIG_ENDIAN | self.data_width)
 
 
         ##Open the device
         if LUN!="": self.open(LUN)
+
+    def __del__(self):
+        self.close()
 
     def open(self, LUN):
         '''Open the device and map to the FPGA bus
@@ -119,8 +175,13 @@ class EthBone(GenDrvr):
     def close(self):
         '''Close the device and unmap
         '''
-        self.lib.eb_device_close(self.device)
-        self.lib.eb_socket_close(self.socket)
+        status=self.lib.eb_device_close(self.device)
+        if status: raise NameError("Close device: %s\n" % (self.eb_status(status)));
+        self.device=c_uint(0)
+
+        status=self.lib.eb_socket_close(self.socket)
+        if status: raise NameError("Close socket: %s\n" % (self.eb_status(status)));
+        self.socket=c_uint(0)
 
 
     def devread(self, bar, offset, width):
@@ -137,11 +198,11 @@ class EthBone(GenDrvr):
         '''
         address = offset
 
-        INTP = POINTER(c_uint)
-        data = c_uint(0xBADC0FFE)
+        INTP = POINTER(c_uint32)
+        data = c_uint32(0xBADC0FFE)
         pData = cast(addressof(data), INTP)
 
-        user_data=c_uint(0)
+        user_data=c_uint32(0)
         cb=c_uint(0)
 
 
@@ -164,9 +225,9 @@ class EthBone(GenDrvr):
             datum : data value that need to be written
         '''
         address = offset
-        data = c_uint(datum)
+        data = c_uint32(datum)
 
-        user_data=c_uint(0)
+        user_data=c_uint32(0)
         cb=c_uint(0)
 
         if self.verbose: print "W@x%08X < 0x%08x" %( address, datum)
@@ -185,22 +246,26 @@ class EthBone(GenDrvr):
             @return The array with the data
         '''
         cycle       = c_uint(0)
-        user_data   = c_uint(0)
-        cb          = c_uint(0)
+        user_data   = c_uint32(0xDEADBEEF)
+        CBFUNC = CFUNCTYPE(None, c_void_p, c_void_p, c_void_p,c_int)
+        cb          = CBFUNC(py_cb_func)
 
-        INTP = POINTER(c_uint)
-        data = c_uint(0xBADC0FFE)
+        INTP = POINTER(c_uint32)
+        data = c_uint32(0xBADC0FFE)
         pData = cast(addressof(data), INTP)
+        pUData = cast(addressof(user_data), INTP)
+
+        print "%s %x %x %x" % (pData,addressof(data),addressof(cb),addressof(user_data))
 
         ldata=[]
         actsize=bsize
         addr=offset
-        status= self.lib.eb_cycle_open(self.device,user_data,cb,self.getPtrData(cycle))
+        status= self.lib.eb_cycle_open(self.device,pUData,cb,self.getPtrData(cycle))
         if status: raise NameError('Cycle open : 0x%x, %s' % (offset,self.eb_status(status)))
         while actsize>0:
-            ##Chequear endianess de format
             self.lib.eb_cycle_read(cycle,addr,self.format,pData)
-            if self.verbose: print "@x%x < %x" % (addr, pData[0])
+            if self.verbose: print "@x%x < %x %s" % (addr, pData[0], pUData[0])
+            break
             ldata.append(pData[0])
             addr=addr+incr
             actsize=actsize-4
@@ -219,8 +284,8 @@ class EthBone(GenDrvr):
         '''
 
         cycle       = c_uint(0)
-        user_data   = c_uint(0)
-        cb          = c_uint(0)
+        user_data   = c_uint32(0)
+        cb          = 0 #NULL pointer
 
 
         addr=offset
@@ -229,9 +294,13 @@ class EthBone(GenDrvr):
         for data in ldata:
             ##Chequear endianess de format
             if self.verbose: print "@x%x > %x" % (addr, data)
-            self.lib.eb_cycle_write(cycle,addr,self.format,c_uint(data))
+            self.lib.eb_cycle_write(cycle,addr,self.format,c_uint32(data))
+            self.wcrc=binascii.crc32(c_uint32(data), self.wcrc)
             addr=addr+incr
-        status=self.lib.eb_cycle_close(cycle)
+        if self.silent:
+            status=self.lib.eb_cycle_close_silently(cycle) #Close without asking acknowledgment of the device (faster)
+        else:
+            status=self.lib.eb_cycle_close(cycle)
         if status: raise NameError('Cycle close: %s' % (self.eb_status(status)))
 
 
@@ -301,19 +370,59 @@ class EthBone(GenDrvr):
         else: print "OK"
 
 
+
+
+
+    def dataToByteArray(self,data_list,bytearray_list):
+        """Method for turning the Data lines strings into byte arrays
+        """
+
+        for i in range(0, len(data_list)):
+            hex_line=data_list[i].decode("hex")
+            hex_array=bytearray(hex_line)
+            bytearray_list.append(hex_array)
+
+        return bytearray_list
+
+
+
+
+    def wordsToPackets(self,data_words,data_packets,packetLen=128):
+        """Method for packing a list of words into a list of packets, each packet of length packetLen
+        """
+        # the 4 bytes words are grouped in "packetLen" words packets
+        for i in range(0,len(data_words),packetLen):
+            if i<(len(data_words)-len(data_words)%packetLen):
+                tmpPacket=[]
+                for k in range(0,packetLen):
+                    tmpPacket.append(data_words[i+k])
+                #tmpPacket.append(0xa0b1c2d4)
+                data_packets.append(tmpPacket)
+            else:
+                last_packet=[]
+                for l in range(0,len(data_words)%packetLen):
+                    last_packet.append(data_words[i+l])
+                data_packets.append(last_packet)
+        return data_packets
+
+
+
     def test_rwblock(self,RAM_offset=0x0):
 
+        print "test R/W block"
         dataw=[]
-        for i in range (0,64):
+        for i in range (0,32):
             dataw.append(i<<24 | i << 16 | i << 8 | i)
 
-        self.devblockwrite(0, RAM_offset, dataw)
+        self.devblockwrite(0, RAM_offset, dataw,4)
         self.read(RAM_offset)
         self.read(RAM_offset+16*4)
         self.read(RAM_offset+32*4)
 
-        datar=self.devblockread(0, RAM_offset, len(dataw))
+        datar=self.devblockread(0, RAM_offset, len(dataw)*4,4)
 
+        return 0
+        print datar
         if datar != dataw:
-            print datar
-            print dataw
+            for i in range(0,len(dataw)):
+                print "%3d x%08x " % (i,dataw[i])
