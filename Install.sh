57d6e018b7380ea60483e44669171ce51659a968
pDAQ group user <pdaq@sdaq01.psl.spts.icecube.wisc.edu>
2022-07-22 15:03:21 +0000

    tweak Install.sh

--- Install.sh
+++ Install.sh
@@ -4,9 +4,9 @@
 # Date: 2016-02-03                                                             #
 # ---------------------------------------------------------------------------- #
 #!/bin/bash
-echo "\033[1mConfiguring py7slib environment...\033[0m"
+echo "Configuring py7slib environment..."
 arch=$(uname -m)
-ln bin/eb-discover_$arch bin/eb-discover
+ln -fv bin/eb-discover_$arch bin/eb-discover
 #ln lib/libetherbone.so.1.0_$arch lib/libetherbone.so.1
 #ln lib/libetherbone.so.1 lib/libetherbone.so
-ln lib/libetherbone.so_$arch lib/libetherbone.so
+ln -fv lib/libetherbone.so_$arch lib/libetherbone.so
